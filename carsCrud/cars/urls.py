47fbdcb071e204f6974cbb9fd07615d1710e1b31
Jok Laurente <jmelaurente@gmail.com>
2020-08-26 16:58:09 +0800

    removed unused urls

--- carsCrud/cars/urls.py
+++ carsCrud/cars/urls.py
@@ -5,12 +5,8 @@ from django.conf.urls import url
 from .views import create_car, list_car, update_car, delete_car
   
 urlpatterns = [ 
-    # path('cars/', view=create_view, name="create_view"),
-    # url(r'^$', views.clup_index, name='clup_index')
     url(r'^$', view=list_car, name="list_car"),
     url(r'create/', view=create_car, name="create_car"),
     url(r'^(?P<id>\d+)/?$', view=update_car, name="update_car"),
     url(r'^(?P<id>\d+)/delete/', view=delete_car, name="delete_car"),
-    # path('appointments/', views.AppointmentList.as_view()),
-    # path('<id>', detail_view ),
 ] 
